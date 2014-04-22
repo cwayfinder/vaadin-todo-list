9df30de8d14e6aee311161011b6cc95b710bedc4
Taras Hupalo <taras.hupalo@gmail.com>
2014-04-22 18:52:20 +0300

    refactored task row with Clara (Vaadin addon)

--- src/main/java/com/way/client/view/widget/TaskRow.java
+++ src/main/java/com/way/client/view/widget/TaskRow.java
@@ -6,87 +6,36 @@ import com.vaadin.event.ShortcutAction;
 import com.vaadin.event.ShortcutListener;
 import com.vaadin.server.UserError;
 import com.vaadin.ui.*;
+import org.vaadin.teemu.clara.Clara;
+import org.vaadin.teemu.clara.binder.annotation.UiField;
+import org.vaadin.teemu.clara.binder.annotation.UiHandler;
 
 /**
  * Created by way on 21.04.2014.
  */
 public class TaskRow extends CustomComponent {
 
-    private final TextField nameField;
-    private final Button edit;
-    private final Button delete;
+    @UiField("doneField")
+    private CheckBox doneField;
+    @UiField("nameField")
+    private TextField nameField;
+    @UiField("deadlineField")
+    private DateField deadlineField;
+    @UiField("edit")
+    private Button edit;
+    @UiField("delete")
+    private Button delete;
 
     public TaskRow(String task) {
-        final HorizontalLayout layout = new HorizontalLayout();
+        setCompositionRoot(Clara.create("/TaskRow.xml", this));
+        addStyleName("task-row");
 
-        final CheckBox doneField = new CheckBox();
-        doneField.addValueChangeListener(new Property.ValueChangeListener() {
-            @Override
-            public void valueChange(Property.ValueChangeEvent event) {
-                if (doneField.getValue()) {
-                    nameField.addStyleName("completed");
-                } else {
-                    nameField.removeStyleName("completed");
-                }
-            }
-        });
-        nameField = new TextField();
         nameField.setValue(task);
-        nameField.addBlurListener(new FieldEvents.BlurListener() {
-            @Override
-            public void blur(FieldEvents.BlurEvent event) {
-                stopEditing();
-            }
-        });
-        nameField.addShortcutListener(new ShortcutListener("",
-                ShortcutAction.KeyCode.ENTER, null) {
-            @Override
-            public void handleAction(Object sender, Object target) {
-                stopEditing();
-            }
-        });
-        edit = new Button("Edit", new Button.ClickListener() {
-            @Override
-            public void buttonClick(Button.ClickEvent event) {
-                startEditing();
-            }
-        });
-        delete = new Button("Delete", new Button.ClickListener() {
-            @Override
-            public void buttonClick(Button.ClickEvent event) {
-                ((ComponentContainer) getParent()).removeComponent(TaskRow.this);
-            }
-        });
-        final DateField deadlineField = new DateField();
-        deadlineField.addValueChangeListener(new Property.ValueChangeListener() {
-            @Override
-            public void valueChange(Property.ValueChangeEvent event) {
-                if (deadlineField.getValue() != null) {
-                    deadlineField.addStyleName("not-empty");
-                } else {
-                    deadlineField.removeStyleName("not-empty");
-                }
-            }
-        });
-
-        layout.addComponents(doneField, nameField, edit, delete, deadlineField);
-        layout.setSpacing(true);
-        layout.addStyleName("task-row");
-        layout.setWidth(100, Unit.PERCENTAGE);
-        nameField.setWidth(100, Unit.PERCENTAGE);
-        layout.setExpandRatio(nameField, 1.0f);
-
-
         nameField.setReadOnly(true);
-
-        setCompositionRoot(layout);
     }
 
     public void stopEditing() {
         nameField.setReadOnly(true);
-//        if (nameField.getValue().length() < 2) {
-//            nameField.setComponentError(new UserError("Must contain 2 symbols at least"));
-//        }
 
         edit.setVisible(true);
         delete.setVisible(true);
@@ -98,5 +47,43 @@ public class TaskRow extends CustomComponent {
 
         edit.setVisible(false);
         delete.setVisible(false);
+    }
+
+    @UiHandler("doneField")
+    public void check(Property.ValueChangeEvent event) {
+        if (doneField.getValue()) {
+            nameField.addStyleName("completed");
+        } else {
+            nameField.removeStyleName("completed");
+        }
+    }
+
+    @UiHandler("nameField")
+    public void valueChange(Property.ValueChangeEvent event) {
+        stopEditing();
+    }
+
+    @UiHandler("nameField")
+    public void blue(FieldEvents.BlurEvent event) {
+        stopEditing();
+    }
+
+    @UiHandler("edit")
+    public void editClick(Button.ClickEvent event) {
+        startEditing();
+    }
+
+    @UiHandler("delete")
+    public void deleteClick(Button.ClickEvent event) {
+        ((ComponentContainer) getParent()).removeComponent(TaskRow.this);
+    }
+
+    @UiHandler("nameField")
+    public void dateChange(Property.ValueChangeEvent event) {
+        if (deadlineField.getValue() != null) {
+            deadlineField.addStyleName("not-empty");
+        } else {
+            deadlineField.removeStyleName("not-empty");
+        }
     }
 }
